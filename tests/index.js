cff0cc855656f1262cf3126f329f8f8f05ea7290
Chris Landa <stylesuxx@gmail.com>
2016-07-06 13:05:46 +0200

    Base Testing structure #2
    Travis should test against Node 6.2
    Added dev dependencies for testing.
    Added npm script to run tests.
    Added failing tests for the main generator and sub generators.

---
+++ tests/index.js
@@ -0,0 +1,23 @@
+const test                        = require('tape');
+const assert                      = require('yeoman-assert');
+const helpers                     = require('yeoman-test');
+
+test('subgenext:app', function(t) {
+  t.plan(1);
+  t.fail('Test not implemented');
+});
+
+test('subgenext:scan --host=x', function(t) {
+  t.plan(1);
+  t.fail('Test not implemented');
+});
+
+test('subgenext:activate foo', function(t) {
+  t.plan(1);
+  t.fail('Test not implemented');
+});
+
+test('subgenext:deactivate foo', function(t) {
+  t.plan(1);
+  t.fail('Test not implemented');
+});
